5185c2f76320a40e1b41c4e24f0716098ced194e
MEIRG <meirg2001@gmail.com>
2026-03-01 16:42:36 +0200

    feat: add date time to add_metadata method

--- metadata_service/handle_file.py
+++ metadata_service/handle_file.py
@@ -1,5 +1,5 @@
 from pathlib import Path 
-
+from datetime import datetime 
 class FileMetadata:
     """add metadata on the file based on its path"""
     def __init__(self, path: str):
@@ -8,13 +8,22 @@ class FileMetadata:
     def add_metadata(self) -> dict:
         """add metadata on the file and return a dict with all the information about the file"""
         name = self.path.name 
-        size = self.path.stat().st_size 
-        print(name)
-        print(size) 
+        stat = self.path.stat()
+        size = stat.st_size 
+        date_created = datetime.fromtimestamp(stat.st_birthtime * 1e-3) 
+        date_modified = datetime.fromtimestamp(stat.st_mtime * 1e-3)
+        
+        
+        return {
+            "name": name, 
+            "size": size, 
+            "date_created": str(date_created), 
+            "date_modified": str(date_modified) 
+            }
 
 FILE = r"C:\Users\MEIRG\Downloads\podcasts\podcasts\download (2).wav"
 file_manager = FileMetadata(FILE)
-file_manager.add_metadata()
+print(file_manager.add_metadata())
     
 
     
