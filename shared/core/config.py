bd5a220e46fce8d9ad1b3a687037cb33438e15f3
MEIRG <meirg2001@gmail.com>
2026-03-02 00:02:18 +0200

    add elastic configuration

--- shared/core/config.py
+++ shared/core/config.py
@@ -5,14 +5,23 @@ class Settings(BaseSettings):
     BOOTSTRAP_SERVERS: str = "localhost:9092"
     METADATA_TOPIC: str = "metadata_topic"
     KAFKA_GROUP_ID: str = ""
-    
+
     # DATA 
     DATA_VOLUME: str = r"C:\Users\MEIRG\Downloads\podcasts_extracted\podcasts"
 
     # MONGODB 
     MONGODB_URL: str = "mongodb://localhost:27017/" 
     MONGO_DB: str = ''
-    MONGO_COLLECTION: str = ""
+    MONGO_COLLECTION: str = "" 
+
+    # elasticsearch 
+    ELASTIC_URL: str = "http://localhost:9200" 
+    ELASTIC_INDEX_NAME: str = '' 
+    
+    
+
+
+settings = Settings()
     
 
 
