4f12baed871c0aca773a2037ce0a4f8e8645d7f1
MEIRG <meirg2001@gmail.com>
2026-03-01 14:48:39 +0200

    docs: add to Settings class configuration the data path

--- shared/core/config.py
+++ shared/core/config.py
@@ -2,5 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     # kafka configuration 
-    KAFKA_ADVERTISED_LISTENERS:str = "PLAINTEXT://localhost:9092"
-    
+    KAFKA_ADVERTISED_LISTENERS:str = "PLAINTEXT://localhost:9092" 
+
+    # DATA 
+    DATA_VOLUME: str = r"C:\Users\MEIRG\Downloads\podcasts\podcasts"
