71b1529ac28e688bf4aa841722eb581776cc67c2
MEIRG <meirg2001@gmail.com>
2026-03-02 00:32:33 +0200

    add class manager of the elastic indexing

--- information_proccessing/elastic.py
+++ information_proccessing/elastic.py
@@ -2,13 +2,14 @@ from elasticsearch import Elasticsearch
 import json 
 
 class ElasticSearch:
-    def __init__(self, index_name, file_metadata: str):
+    def __init__(self, index_name, file_metadata: str, hosts):
         self.index_name = index_name 
         self.metadata: dict = json.loads(file_metadata)
+        self.elastic: Elasticsearch = Elasticsearch(hosts=hosts)
 
-    def create_index(self, elastic: Elasticsearch):
+    def create_index(self, ):
         id = self.metadata.get('id', '')
-        index = elastic.index(index=self.index_name, document=self.metadata, id=id) 
+        index = self.elastic.index(index=self.index_name, document=self.metadata, id=id) 
         print(f"the index added successfully {str(index)} ")
         
 
