cb3a0da14c15e777d80458788f52dabd90bab090
MEIRG <meirg2001@gmail.com>
2026-03-01 15:43:51 +0200

    docs: add method to FileMetadata class for adding the name and size

--- metadata_service/handle_file.py
+++ metadata_service/handle_file.py
@@ -5,7 +5,16 @@ class FileMetadata:
     def __init__(self, path: str):
         self.path: Path = Path(path) 
 
+    def add_metadata(self) -> dict:
+        """add metadata on the file and return a dict with all the information about the file"""
+        name = self.path.name 
+        size = self.path.stat().st_size 
+        print(name)
+        print(size) 
 
+FILE = r"C:\Users\MEIRG\Downloads\podcasts\podcasts\download (2).wav"
+file_manager = FileMetadata(FILE)
+file_manager.add_metadata()
     
 
     
--- metadata_service/main.py
+++ metadata_service/main.py
@@ -1,6 +1,7 @@
-from handle_file import FileMetadata 
+from .handle_file import FileMetadata 
 from shared.core.config import settings
 class Manager:
     def __init__(self):
         self.folder = settings.DATA_VOLUME 
+        
 
