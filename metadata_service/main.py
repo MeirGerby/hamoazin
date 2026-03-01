c1490a5637097f928385ae4088569ec77076992b
MEIRG <meirg2001@gmail.com>
2026-03-01 16:51:06 +0200

    docs: add topic named metadata topic to config file and initialize it in the Manager class

--- metadata_service/main.py
+++ metadata_service/main.py
@@ -4,7 +4,11 @@ from .handle_file import FileMetadata
 class Manager:
     def __init__(self):
         self.folder = settings.DATA_VOLUME 
+        self.metadata_topic = settings.METADATA_TOPIC
+        self.producer = None
 
-    
+    async def setup(self):
+        pass 
+
         
 
--- shared/core/config.py
+++ shared/core/config.py
@@ -3,10 +3,10 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     # kafka configuration 
     KAFKA_ADVERTISED_LISTENERS:str = "PLAINTEXT://localhost:9092" 
-
+    METADATA_TOPIC: str = "metadata_topic"
     # DATA 
     DATA_VOLUME: str = r"C:\Users\MEIRG\Downloads\podcasts\podcasts"
-
+    
 
 
 settings = Settings()
