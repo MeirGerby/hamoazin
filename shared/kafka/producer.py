d0642bc008051e1dca5654cf5c9f138b14bdaced
MEIRG <meirg2001@gmail.com>
2026-03-02 13:31:23 +0200

    refactor: remove commented print

--- shared/kafka/producer.py
+++ shared/kafka/producer.py
@@ -12,16 +12,13 @@ class ProducerMessages:
     def delivary_report(self, err, msg):
         """report whether the delively succeed or not"""
         if err is not None:
-            # print(f"Delivary failed {err}")
             logger.error(f"Delivary failed {err}") 
         else:
-            # print(f"the msg send successfully {msg.value().decode("utf-8")}")
             logger.info(f"the msg send successfully {msg.value().decode("utf-8")}")  
     
     async def send_messege(self, message: dict): 
         """end a messege to kafka """
         try:
-            # print(f"send message to {self.topic}")
             logger.info(f"send message to {self.topic}")  
 
             self.producer.produce(
@@ -30,7 +27,10 @@ class ProducerMessages:
                 on_delivery=self.delivary_report
             )
             self.producer.poll(0) 
-            logger.info(f"the message send successfully,\n file's metadata: {message} \n topic: {self.topic}")
+            logger.info(
+                f"the message send successfully,\n"
+                f"file's metadata: {message} \n topic: {self.topic}"
+                )
 
         except Exception as e:
             logger.exception(e)
@@ -39,7 +39,6 @@ class ProducerMessages:
         """close the connection to kafka"""
         remaining = self.producer.flush(10)
         if remaining > 0:
-            # print(f"Warrning: {remaining} the messages were not delivered")
             logger.warning(f"Warrning: {remaining} the messages were not delivered")
         else:
             logger.info("all messages delivered successfully! ")
