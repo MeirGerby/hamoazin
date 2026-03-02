1ac3bf86e74ad2caff5c6f43ef8a8d95630abd86
MEIRG <meirg2001@gmail.com>
2026-03-02 12:48:01 +0200

    feat: add elasticsearch logging functionality and new index configuration

--- shared/kafka/consumer.py
+++ shared/kafka/consumer.py
@@ -1,6 +1,8 @@
 from confluent_kafka import Consumer 
 import json
-# import logging 
+from logs.logs import Logger 
+
+logger = Logger.get_logger(__name__)
 
 class ConsumerMessages:
     def __init__(self, bootstrap_servers: str, topics: list, group_id: str):
