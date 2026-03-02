790db05549025403b8dc66eed7d9e2f892ae35ea
MEIRG <meirg2001@gmail.com>
2026-03-02 09:40:12 +0200

    refactor: add group id and mongodb name

--- shared/core/config.py
+++ shared/core/config.py
@@ -4,19 +4,19 @@ class Settings(BaseSettings):
     # kafka configuration 
     BOOTSTRAP_SERVERS: str = "localhost:9092"
     METADATA_TOPIC: str = "metadata_topic"
-    KAFKA_GROUP_ID: str = ""
+    KAFKA_GROUP_ID: str = "metadata_consumer_group"
 
     # DATA 
     DATA_VOLUME: str = "/data/podcasts"
 
     # MONGODB 
     MONGODB_URL: str = "mongodb://localhost:27017/" 
-    MONGO_DB: str = ''
-    MONGO_COLLECTION: str = "" 
+    MONGO_DB: str = 'podcasts_db'
+    MONGO_COLLECTION: str = "podcasts" 
 
     # elasticsearch 
     ELASTIC_URL: str = "http://localhost:9200" 
-    ELASTIC_INDEX_NAME: str = '' 
+    ELASTIC_INDEX_NAME: str = 'podcasts' 
     
     
 
