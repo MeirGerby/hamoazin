971b2f8cb7ca58c13f2999cf229fcef116820aff
MEIRG <meirg2001@gmail.com>
2026-03-04 22:53:00 +0200

    refactor: update Singleton __new__ method to accept arguments

--- transcription_computer/recognition_singleton.py
+++ transcription_computer/recognition_singleton.py
@@ -3,7 +3,7 @@ import speech_recognition as sr
 class Singleton:
     _instance = None 
 
-    def __new__(cls):
+    def __new__(cls, *args, **kwargs):
         if not cls._instance:
             cls._instance = super(Singleton, cls).__new__(cls)
             cls._instance.r = sr.Recognizer()  # type: ignore
